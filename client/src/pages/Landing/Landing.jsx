c899426d926f14c8f5d1afd59b8377a79ebf5ad3
Vishal Pratap Singh <vishal19111999@hotmail.com>
2021-01-24 22:08:57 +0530

    refactor(components): separated login button

--- client/src/pages/Landing/Landing.jsx
+++ client/src/pages/Landing/Landing.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./Landing.css";
 import dd_logo from "../../common_assets/logo.png";
-import github_logo from "../../common_assets/github_logo.png";
+import GithubLoginButton from "../../components/GithubLoginButton/GithubLoginButton";
 
 export default function Landing() {
   return (
@@ -11,19 +11,7 @@ export default function Landing() {
         <span className={"landing-heading"}>
           India's hub for supporting <br /> open source
         </span>
-        <div className={"github-login-button"}>
-          <div>
-            <img
-              src={github_logo}
-              style={{ width: "1.6em", marginRight: "0.9em" }}
-            ></img>
-          </div>
-          <div style={{ marginTop: "0.2em" }}>
-            <span>
-              <b>Login with GitHub</b>
-            </span>
-          </div>
-        </div>
+        <GithubLoginButton/>
       </div>
       <div className={"landing-footer"}></div>
     </div>
