d70aaedf001e25bc948413e0353f6cce1a9071d7
Vishal Pratap Singh <vishal19111999@hotmail.com>
2021-01-24 18:39:36 +0530

    feat(pages/landing): added landing page

---
+++ client/src/pages/Landing/Landing.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Landing.css";
+
+export default function Landing() {
+  return (
+    <div className={"landing-container"}>
+      <div className={"landing-main"}>
+        <span style={{ color: "white" }}>India's hub for open source</span>
+      </div>
+      <div className={"landing-footer"}></div>
+    </div>
+  );
+}
