f8bc8ebbb5d26724d9dcb6298497232a7330a55e
Vishal Pratap Singh <vishal19111999@hotmail.com>
2021-01-24 21:49:53 +0530

    update(pages/landing): updated the landing page

--- client/src/pages/Landing/Landing.jsx
+++ client/src/pages/Landing/Landing.jsx
@@ -1,11 +1,29 @@
 import React from "react";
 import "./Landing.css";
+import dd_logo from "../../common_assets/logo.png";
+import github_logo from "../../common_assets/github_logo.png";
 
 export default function Landing() {
   return (
     <div className={"landing-container"}>
       <div className={"landing-main"}>
-        <span style={{ color: "white" }}>India's hub for open source</span>
+        <img src={dd_logo} className={"landing-logo"}></img>
+        <span className={"landing-heading"}>
+          India's hub for supporting <br /> open source
+        </span>
+        <div className={"github-login-button"}>
+          <div>
+            <img
+              src={github_logo}
+              style={{ width: "1.6em", marginRight: "0.9em" }}
+            ></img>
+          </div>
+          <div  style={{marginTop:"0.18em"}}>
+            <span>
+              <b>Login with GitHub</b>
+            </span>
+          </div>
+        </div>
       </div>
       <div className={"landing-footer"}></div>
     </div>
