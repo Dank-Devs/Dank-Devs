5abc8b427b43a167c01140a217ae5bfadb6a363d
Vishal Pratap Singh <vishal19111999@hotmail.com>
2021-01-26 02:56:04 +0530

    feat(pages/profile):added repo cards

--- client/src/pages/Profile/Profile.jsx
+++ client/src/pages/Profile/Profile.jsx
@@ -3,27 +3,35 @@ import "./Profile.css";
 import dummy_profile_pic from "./../../common_assets/dummy_profile_pic.jpg";
 import { connect } from "react-redux";
 
-const mapStateToProps = (state)=>{
-  return{
-    name:state.name,
-    id:state.id
-  }
+const mapStateToProps = (state) => {
+  return {
+    name: state.name,
+    id: state.id,
+    repos: state.repos,
+  };
 };
 
 export default connect(mapStateToProps)(function Profile(props) {
   function render_repos() {
-    return <div></div>;
+    return props.repos.map((repo) => (
+      <div className={"profile-repo-card"}>{repo}</div>
+    ));
   }
+
   console.log(props);
+
   return (
     <div className={"profile-container"}>
       <div className={"profile-main"}>
-        <img className={"profile-pic"} src={dummy_profile_pic}></img>
+        <img className={"profile-pic"} src={dummy_profile_pic} alt={"Profile Pic"}></img>
         <div className={"profile-name"}>
           <span>{props.name}</span>
         </div>
         <div className={"profile-id"}>
-          <span>{"@"+props.id}</span>
+          <span>{"@" + props.id}</span>
+        </div>
+        <div style={{ marginBottom: "10vh", marginTop: "5vh" }}>
+          {render_repos()}
         </div>
       </div>
     </div>
