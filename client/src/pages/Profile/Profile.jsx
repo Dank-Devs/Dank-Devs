fefeb5e26300cbf2b9e51d5ee3fc27951d49a476
Vishal Pratap Singh <vishal19111999@hotmail.com>
2021-01-26 00:22:31 +0530

    feat(redux): added redux and created a reducer

--- client/src/pages/Profile/Profile.jsx
+++ client/src/pages/Profile/Profile.jsx
@@ -1,9 +1,31 @@
 import React from "react";
 import "./Profile.css";
+import dummy_profile_pic from "./../../common_assets/dummy_profile_pic.jpg";
+import { connect } from "react-redux";
 
-export default function Profile() {
+const mapStateToProps = (state)=>{
+  return{
+    name:state.name,
+    id:state.id
+  }
+};
+
+export default connect(mapStateToProps)(function Profile(props) {
+  function render_repos() {
+    return <div></div>;
+  }
+  console.log(props);
   return (
-  <div className={"profile-container"}>
-      <span>profile page</span>
-  </div>);
-}
+    <div className={"profile-container"}>
+      <div className={"profile-main"}>
+        <img className={"profile-pic"} src={dummy_profile_pic}></img>
+        <div className={"profile-name"}>
+          <span>{props.name}</span>
+        </div>
+        <div className={"profile-id"}>
+          <span>{"@"+props.id}</span>
+        </div>
+      </div>
+    </div>
+  );
+});
