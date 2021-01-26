c156d7ca6733c9aab1c7da4592134ca96a96d538
Ankit Raj <ankitatiiitr@gmail.com>
2021-01-27 01:35:39 +0530

    added auth

--- client/src/components/GithubLoginButton/GithubLoginButton.jsx
+++ client/src/components/GithubLoginButton/GithubLoginButton.jsx
@@ -3,8 +3,11 @@ import github_logo from "../../common_assets/github_logo.png";
 import "./GithubLoginButton.css";
 
 export default function GithubLoginButton() {
+  const authEndpoint = `https://github.com/login/oauth/authorize?client_id=${process.env.REACT_APP_CLIENT_ID}&redirect_uri=${process.env.REACT_APP_REDIRECT_URI}&scope=user`;
   return (
-    <div className={"github-login-button"}>
+    <div className={"github-login-button"} onClick={() => {
+      window.location = authEndpoint;
+    }}>
       <div>
         <img
           src={github_logo}
