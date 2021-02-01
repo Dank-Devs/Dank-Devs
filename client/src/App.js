ff550ba1c5f99281cb1cd88537301a7a1d6e3213
Vishal Pratap Singh <vishal19111999@hotmail.com>
2021-02-02 01:55:34 +0530

    update(pages/profile): updated repo cards

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,6 @@ import "./App.css";
 import Landing from "./pages/Landing/Landing";
 import Profile from "./pages/Profile/Profile";
 import { Route, BrowserRouter } from "react-router-dom";
-import axios from "axios";
 
 function App() {
   return (
