d8143bbf8e4c0fd1ade7d8f7bcb4e6cc9e128025
Ankit Raj <ankitatiiitr@gmail.com>
2021-02-03 03:23:10 +0530

    (feat): Feed setup

--- client/src/App.js
+++ client/src/App.js
@@ -2,17 +2,17 @@ import "./App.css";
 import Landing from "./pages/Landing/Landing";
 import Profile from "./pages/Profile/Profile";
 import { Route, BrowserRouter } from "react-router-dom";
+import Nav from "./components/Nav/Nav";
+import Feed from "./pages/Feed/Feed";
 
 function App() {
   return (
       <div className="App">
         <BrowserRouter>
-          <Route exact path="/">
-            <Landing />
-          </Route>
-          <Route path="/profile">
-            <Profile />
-          </Route>
+          <Nav />
+          <Route exact path="/" component={Landing} />
+          <Route exact path="/profile/:username" component={Profile} />
+          <Route exact path="/feed" component={Feed} />
         </BrowserRouter>
       </div>
   );
